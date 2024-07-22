42a03c0f73c432d7b22adb0cbe34b98a0db7c874
GRossMcDonald <ross_mcdonald@outlook.com>
2024-07-21 20:18:55 -0400

    got an image to appear with some basic frontend

--- backend/src/main/java/com/wcci/jacenterprises/repository/JacRepository.java
+++ backend/src/main/java/com/wcci/jacenterprises/repository/JacRepository.java
@@ -1,7 +1,6 @@
 package com.wcci.jacenterprises.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.wcci.jacenterprises.entity.Jac;
 
 public interface JacRepository extends JpaRepository <Jac, Long> {
