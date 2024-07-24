999a427ba3e06003165500e7a7e2e4f09ade4675
ChrisWCCI <chrisryan@wecancodeit.org>
2024-07-24 19:43:48 -0400

    rename app

--- backend/src/main/java/com/wcci/jacenterprises/JacenterpriseApplication.java
+++ backend/src/main/java/com/wcci/jacenterprises/JacenterpriseApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JacenterprisesApplication {
+public class JacenterpriseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JacenterprisesApplication.class, args);
+		SpringApplication.run(JacenterpriseApplication.class, args);
 	}
 
 }
