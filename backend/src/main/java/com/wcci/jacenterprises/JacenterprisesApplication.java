8bec37dce6364b4d86237a26f523a941653a778b
ChrisWCCI <chrisryan@wecancodeit.org>
2024-07-24 19:45:28 -0400

    give up renaming

--- backend/src/main/java/com/wcci/jacenterprises/JacenterprisesApplication.java
+++ backend/src/main/java/com/wcci/jacenterprises/JacenterprisesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JacEnterprise {
+public class JacenterprisesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JacEnterprise.class, args);
+		SpringApplication.run(JacenterprisesApplication.class, args);
 	}
 
 }
