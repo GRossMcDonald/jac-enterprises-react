38a3722fec0a58b51e0dfa8ce3bfb0bf0dc8be3d
IgnorantOracle <deidra.clay15@gmail.com>
2024-07-24 20:09:50 -0400

    testing push ability

--- frontend/src/components/Funnies.js
+++ frontend/src/components/Funnies.js
@@ -3,9 +3,27 @@ import Navbar from "./Navbar";
 import "./Navbar.css";
 
 function Funnies() {
+  const fetchJokes = ({ setFavorites }) => {
+    fetch("http://localhost:8080/api/jokes")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not okay.");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setFavorites(data);
+      })
+      .catch((error) => {
+        console.error(
+          "There was an issue fetching the favorites jokes: " + error
+        );
+      });
+  };
   return (
     <div className="Funnies">
       <Navbar />
+      <button onClick={fetchJokes}>Show Favorite Jokes</button>
     </div>
   );
 }
