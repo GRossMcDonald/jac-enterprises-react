728b2e8997bc9333be9b206f5b21b57b8dbbb113
Kate Locke <168797943+klocke1028@users.noreply.github.com>
2024-07-25 13:57:41 -0400

    Contact Page tweaks

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -1,38 +1,33 @@
 import React from "react";
 import "./Contact.css";
 import Navbar from "./Navbar";
-import Background from "./Background";
 import "./Background.css";
 
 function Contact() {
   return (
     <div className="contact-container">
       <Navbar />
-      <Background />
       <div className="contact-info">
-        <h1 className="contact-header">Contact Us!</h1>
-        <div className="contact-list">
-          <ul>
-            <li className="contact-item">
-              Deidra Zamonski-Blake:
-              <a href="deidra.zamonskiblake@gmail.com" className="email-link">
-                Send E-mail
-              </a>
-            </li>
-            <li className="contact-item">
-              Ross McDonald:
-              <a href="mailto:Ross_McDonald@gmail.com" className="email-link">
-                Send E-mail
-              </a>
-            </li>
-            <li className="contact-item">
-              Katlyn Locke:
-              <a href="mailto:katlynlocke1028@gmail.com" className="email-link">
-                Send E-Mail
-              </a>
-            </li>
-          </ul>
-        </div>
+        <ul>
+          <li>
+            <h2>Deidra Zamonski-Blake</h2>
+            <a href="deidra.zamonskiblake@gmail.com" className="email-link">
+              Send E-mail
+            </a>
+          </li>
+          <li>
+            <h2>Ross McDonald:</h2>
+            <a href="mailto:Ross_McDonald@outlook.com" className="email-link">
+              Send E-mail
+            </a>
+          </li>
+          <li>
+            <h2>Katlyn Locke</h2>
+            <a href="mailto:katlynlocke1028@gmail.com" className="email-link">
+              Send E-Mail
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
   );
