a893b816d6f74a26d4d0d80388c8bd13f5c60a5a
Kate Locke <168797943+klocke1028@users.noreply.github.com>
2024-07-25 15:06:05 -0400

    About changes

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,25 +1,46 @@
 import React from "react";
 import "./About.css";
 import Navbar from "./Navbar";
-import Background from "./Background";
+import "./Background.css";
 
 function About() {
   return (
     <div className="bio-container">
       <Navbar />
-      <Background />
-      <h1>Meet Our Team!</h1>
-      <div className="Deidra">
-        <h2>Deidra Zamonski-Blake</h2>
-        <p>Bio filler</p>
-      </div>
-      <div className="Ross">
-        <h2>Ross McDonald</h2>
-        <p>Bio Filler</p>
-      </div>
-      <div className="Kate">
-        <h2>Kate Locke</h2>
-        <p>Bio Filler</p>
+      <div className="about-info">
+        <h1>Meet Our Team!</h1>
+        <div className="Deidra">
+          <h2>Deidra Zamonski-Blake</h2>
+          <img className="bio-pic" src="../img/Deidra.png" alt="Deidra"></img>
+          <p>
+            Deidra is an entry-level full-stack web developer located in
+            Columbus, OH. She and her husband of three years, Christopher, had
+            their first child, Beorn, at the end of 2023 and she will soon be a
+            new graduate of the trade school We Can Code It.
+          </p>
+        </div>
+        <div className="Ross">
+          <h2>Ross McDonald</h2>
+          <img className="bio-pic" src="./img/Ross.png" alt="Ross"></img>
+          <p>
+            Ross is a We Can Code It student with a background in political
+            science. He graduated from OSU with a bachelors in political
+            science. Since then he's aspired to become a video game developer.
+            He's currently honing my skills in backend and frontend programming
+            languages.
+          </p>
+        </div>
+        <div className="Kate">
+          <h2>Kate Locke</h2>
+          <p>
+            Kate is a full-stack development student at We Can Code it with a
+            background in communication studies. In 2017 she graduated from Kent
+            State University and has since found an interest in programming. She
+            is set to graduate from WCCI in August of 2024. She is hoping to
+            find a career where she can dive more into front-end development in
+            the gaming or entertainment industries.
+          </p>
+        </div>
       </div>
     </div>
   );
