07df6b17eb156dd943117620ef5854ff92cb41d2
IgnorantOracle <deidra.clay15@gmail.com>
2024-07-24 21:39:40 -0400

    edited logo in navbar on explore page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { useState } from "react";
 import "./components/Home.css";
 import "./components/Funnies.css";
 import Funnies from "./components/Funnies";
 import Home from "./components/Home";
+import Explore from "./components/Explore";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
+  const [nasaTitle, setTitle] = useState("");
+  const [nasaImage, setImage] = useState("");
+  const [nasaCaption, setCaption] = useState("");
+
   return (
     <>
       <div className="background-container"></div>
@@ -13,10 +19,20 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/funnies" element={<Funnies />} />
-
-          {/*
-              <Route path="/explore" element={<Element />} />
-              
+          <Route
+            path="/explore"
+            element={
+              <Explore
+                setTitle={setTitle}
+                setImage={setImage}
+                setCaption={setCaption}
+                nasaTitle={nasaTitle}
+                nasaImage={nasaImage}
+                nasaCaption={nasaCaption}
+              />
+            }
+          />
+          {/* 
               <Route path="/contact" element={<Contact />} />
               <Route path="/about" element={<About />} />
              */}
